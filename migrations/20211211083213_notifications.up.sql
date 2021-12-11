94f25a092a02bf134b9dfff64053e647fd61ce7e
I0HuKc <web-disain@mail.ua>
2021-12-11 14:11:54 +0300

    notifacation

---
+++ migrations/20211211083213_notifications.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE notifications(
+    id BIGSERIAL NOT NULL PRIMARY KEY,    
+    type INT NOT NULL,
+    status INT DEFAULT 1,
+    chat_id BIGINT NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    code INT,
+    user_card VARCHAR(255),
+    created_at TIMESTAMP DEFAULT now(),
+    updated_at TIMESTAMP DEFAULT now()
+);
