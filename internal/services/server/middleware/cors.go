b4c783292393f7963e48577cb7c07d36ef1c22ad
I0HuKc <web-disain@mail.ua>
2021-12-26 11:07:19 +0300

    fix CORS

--- internal/services/server/middleware/cors.go
+++ internal/services/server/middleware/cors.go
@@ -2,6 +2,22 @@ package middleware
 
 import "github.com/gin-gonic/gin"
 
+func (m *Middleware) CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func (m *Middleware) SetCors() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Add("Access-Control-Allow-Origin", "*")
--- internal/services/server/middleware/middleware.go
+++ internal/services/server/middleware/middleware.go
@@ -5,6 +5,7 @@ import "github.com/gin-gonic/gin"
 type Middleware struct{}
 
 type MiddlewareI interface {
+	CORSMiddleware() gin.HandlerFunc
 	SetCors() gin.HandlerFunc
 }
 
