ada109f0fb652cdac12810a16b43ae89d6a4e449
I0HuKc <web-disain@mail.ua>
2022-01-12 12:39:25 +0300

    Сделал сохранение полезной нагрузки в лог

--- internal/services/server/guard/logging.go
+++ internal/services/server/guard/logging.go
@@ -1,6 +1,8 @@
 package guard
 
 import (
+	"bytes"
+	"io/ioutil"
 	"net/http"
 
 	AppType "github.com/gefion-tech/tg-exchanger-server/internal/core/types"
@@ -16,25 +18,22 @@ func (g *Guard) Logger(resource, action string) gin.HandlerFunc {
 			return
 		}
 
-		// // var body map[string]interface{}
-		// // if err := c.ShouldBindJSON(&body); err != nil {
-		// // 	fmt.Println(err)
-		// // 	return
-		// // }
+		var bodyBytes []byte
+		if c.Request.Body != nil {
+			bodyBytes, err = ioutil.ReadAll(c.Request.Body)
+			if err != nil {
+				g.responser.Error(c, http.StatusInternalServerError, err)
+				return
+			}
+		}
 
-		// // jsonString, err := json.Marshal(body)
-		// // if err != nil {
-		// // 	fmt.Println(err)
-		// // }
-		// b := c.Request.Body
-		// jsonData, err := ioutil.ReadAll(c.Request.Body)
-
-		// fmt.Println(string(jsonData))
+		// Возвращаю io.ReadCloser в исходное состояние.
+		c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
 
 		go g.logger.NewRecord(&models.LogRecord{
 			Service:  AppType.LogTypeAdmin,
 			Module:   resource,
-			Info:     c.Request.Body,
+			Info:     string(bodyBytes),
 			Username: &token.Username,
 		})
 
