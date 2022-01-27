5b28970a644da1df778e07544735bfdcd3770c1c
I0HuKc <web-disain@mail.ua>
2022-01-27 18:59:25 +0300

    Убрал табуляцию

--- internal/utils/stepper.go
+++ internal/utils/stepper.go
@@ -8,18 +8,18 @@ import (
 
 func SetSuccessStep(msg string) {
 	gb := color.New(color.FgGreen, color.Bold)
-	fmt.Printf(fmt.Sprintf("%s...\t", msg))
+	fmt.Printf(fmt.Sprintf("%s... ", msg))
 	gb.Printf("ok\n")
 }
 
 func SetAttentionStep(msg string) {
 	yb := color.New(color.FgYellow, color.Bold)
-	fmt.Printf(fmt.Sprintf("%s...\t", msg))
+	fmt.Printf(fmt.Sprintf("%s...", msg))
 	yb.Printf("attention\n")
 }
 
 func SetErrorStep(msg string) {
 	rb := color.New(color.FgRed, color.Bold)
-	fmt.Printf(fmt.Sprintf("%s...\t", msg))
+	fmt.Printf(fmt.Sprintf("%s...", msg))
 	rb.Printf("error\n")
 }
