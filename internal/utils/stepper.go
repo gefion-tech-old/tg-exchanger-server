119724c7869d7ad837cd5d2317fac4a524bdf175
I0HuKc <web-disain@mail.ua>
2022-01-29 18:28:09 +0300

    Изменил тип принимаемого значения

--- internal/utils/stepper.go
+++ internal/utils/stepper.go
@@ -4,21 +4,22 @@ import (
 	"fmt"
 
 	"github.com/fatih/color"
+	AppType "github.com/gefion-tech/tg-exchanger-server/internal/core/types"
 )
 
-func SetSuccessStep(msg string) {
+func SetSuccessStep(msg AppType.AppStep) {
 	gb := color.New(color.FgGreen, color.Bold)
 	fmt.Printf(fmt.Sprintf("%s... ", msg))
 	gb.Printf("ok\n")
 }
 
-func SetAttentionStep(msg string) {
+func SetAttentionStep(msg AppType.AppStep) {
 	yb := color.New(color.FgYellow, color.Bold)
 	fmt.Printf(fmt.Sprintf("%s...", msg))
 	yb.Printf("attention\n")
 }
 
-func SetErrorStep(msg string) {
+func SetErrorStep(msg AppType.AppStep) {
 	rb := color.New(color.FgRed, color.Bold)
 	fmt.Printf(fmt.Sprintf("%s...", msg))
 	rb.Printf("error\n")
